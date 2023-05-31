6938f1cdbccb1fabc548502423ed7ed06a0efd04
Carl Frenn <carl.frenn@gmail.com>
2023-05-31 21:34:53 +0200

    moved test to tests folder

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -2,11 +2,11 @@ import { describe, it, expect } from 'vitest';
 
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import App from '../src/App';
 
 describe('App', () => {
   it('renders headline', () => {
-    render(<App title="React" />);
+    render(<App />);
 
     screen.debug();
 
